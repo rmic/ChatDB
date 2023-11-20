af4b9bd95fec6a45da63345876a429d19ad87f15
Sarah Pinon <95487702+sapinon@users.noreply.github.com>
2023-11-20 16:32:56 +0100

    Update FR version, prompt changes
    - Traduit certains textes qui restaient en anglais sur l'interface
    - changer des prompts: Data Catalog content, recommandation de questions qui sont traitables par l'outil.

---
+++ ChatDB-main/chainlit.md
@@ -0,0 +1,66 @@
+# Welcome to B2Data!
+
+Désireux d'explorer le potentiel de B2Data ?
+ 
+Voici toutes les informations dont je dispose pour répondre à vos besoins...
+
+En résumé, 
+Ce chatbot a accès aux données 2023 concernant les admissions à l'hôpital, les informations personnelles des patients, les services où se trouvent les patients, les diagnostics effectués sur les patients, les analyses de laboratoire, les interventions et les prescriptions de médicaments.
+
+Concrètement,
+
+Informations sur les admissions
+-------------------------------
+• La date et l'heure d'admission du patient au sein de l’hôpital et de sortie.
+
+• Le type d’admission (Ambulatory observation, Direct Emer, Direct observation, Elective, EU observation, EW emer, Observation Admit, Surgical Same Day Admission, Urgent) 
+
+• La raison de l'admission
+
+• L'assurance du patient
+
+• La langue parlée par le patient
+
+• L'état civil du patient
+
+• L'ethnie du patient
+
+• Une indication sur le décès du patient au sien de l’hôpital ou non
+
+• Le service dans lequel le patient a été admis
+  
+Informations sur les patients
+-----------------------------
+• Les informations démographiques de base, à savoir : le genre, l’âge, et l’éventuel date de mort du patient.
+
+Informations sur les diagnostiques
+----------------------------------
+• Les diagnostics associés au séjour des patients à l’hôpital.
+
+Informations sur les analyses de laboratoire
+--------------------------------------------
+• Le type d’échantillon utilisé
+
+• La date et l'heure auxquelles l’échantillon a été enregistré
+
+• La date et l'heure auxquelles les résultats de l’analyse sont disponibles
+
+• Le résultat anormal ou non de l’analyse 
+
+• Le niveau de priorité de l’analyse
+
+Informations sur les interventions
+----------------------------------
+• Les interventions réalisées sur les patients de l’hôpital (e.g. Bypass)
+
+• Leur date 
+
+Informations sur les administrations de médicaments
+---------------------------------------------------
+• Les médicaments administrés aux patients
+
+• Une description des médicaments
+
+• La chronologie des administrations
+
+• L’heure à laquelle le médicament a été administré 
---
+++ ChatDB-main/chatbot/prompts.py
@@ -0,0 +1,82 @@
+from langchain.prompts.prompt import PromptTemplate
+
+CYPHER_GENERATION_TEMPLATE = """Task:Generate Cypher statement to query a graph database. 
+Schema:
+{schema}
+Instructions:
+Execute one or more queries to answer the user's question please only generate cypher code that make use of existing node types and relationships and pay a special attention to the direction of the relationships.
+
+Note: Do make use of the existing relationships
+Start by analyzing the graph schema, then build your cypher query around it. 
+Data names used in the query must exactly refer to data names present in the graph.
+If a part of your query is based on dates or time frames, take into account that today's date is {today}
+Do not include any explanations or apologies in your responses.
+Do not respond to any questions that might ask anything else than for you to construct a Cypher statement.
+Do not include any text except the generated Cypher statement.
+
+Concerning the database, all data fields concerning time information (charttime, admittime, storetime, admittime, dischtime,...) provides the date (year, month and day) and time of the event. 
+The 'gender' data field in 'Patient' nodes provides the gender of the patient which is female when the value is 'F' and male when it's 'M'.
+The 'hospital_expire_flag' data field indicates whether the patient died within the hospital by a value 1 or not vy a value 0. 
+The 'description' data field in the 'Disease' nodes provides the meaning of the disease ICD code.
+The 'LabItem' nodes represents a laboratory concept. The 'LabAnalysis' nodes provides information about the laboratory analysis made on these concepts. 
+The 'Procedure' notes represents medical interventions which are described via the 'ProcedureDescription' nodes. These nodes contains the name of the procedures.
+
+The question is:
+{question}"""
+
+CYPHER_GENERATION_PROMPT = PromptTemplate(
+    input_variables=["schema", "today", "question"], template=CYPHER_GENERATION_TEMPLATE
+)
+
+PREFIX = """Répondez aux questions suivantes du mieux que vous pouvez. Vous avez accès aux outils suivants:"""
+#Ne pas changer les intitulés "Action", "Observation",...
+FORMAT_INSTRUCTIONS = """Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{tool_names}]. If there is no action to take, simply return the final answer to the user without trying to use a tool.
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question, IN FRENCH"""
+SUFFIX = """Begin!
+
+Question: {input}
+Thought:{agent_scratchpad}"""
+#Changer ici si modif ChatDB
+CONVERSATION_TEMPLATE = """Vous êtes l'assistant d'un humain travaillant dans un hôpital, grâce à un grand modèle linguistique entraîné par OpenAI.
+Vous êtes conçu pour être en mesure de répondre à des questions sur le monde médical et plus particulièrement, sur les données de l'hôpital, auxquelles vous avez accès dans la base de données Noe4J. 
+Vous apprenez et vous vous améliorez constamment, et vos capacités évoluent sans cesse.
+Vous êtes capable de traiter et de comprendre de grandes quantités de texte et d'utiliser ces connaissances pour fournir des réponses précises et informatives à un large éventail de questions. 
+Vous avez accès à certaines informations personnalisées fournies par l'utilisateur dans la section Contexte ci-dessous. 
+Vous construisez votre réponse sur la base des données situées dans la base de données Neo4J à laquelle vous avez accès et vous fournissez des références et des informations à l'utilisateur sur la manière dont vous avez produit les résultats.
+Ne générez pas de db requêtes, mais formulez la question le mieux possible et utilisez l'outil "Intermediate Answer" à votre disposition pour interroger la base de données.
+Si vous ne trouvez pas l'information dans la base de données, dites-le et ne donnez jamais de réponses arbitraires à propos des données.
+La section Profil utilisateur ci-dessous décrit l'utilisateur humain de manière plus détaillée, et vous devez adapter votre réponse à ce profil d'utilisateur et utiliser une langue correspondante.
+La date d'aujourd'hui est : {today}
+Vos interactions avec l'utilisateur doivent être en français.
+
+Profil utilisateur:
+{user_profile}
+
+Contexte:
+{entities}
+
+Current conversation:
+{history}
+
+"""
+
+def create_prompt(tools):
+    tool_strings = "\n".join([f"{tool.name}: {tool.description}" for tool in tools])
+    tool_names = ", ".join([tool.name for tool in tools])
+    format_instructions = FORMAT_INSTRUCTIONS.format(tool_names=tool_names)
+    zero_shot_template = "\n\n".join([PREFIX, tool_strings, format_instructions, SUFFIX])
+
+    chat_db_prompt = PromptTemplate(
+        input_variables=["entities", "history", "agent_scratchpad", "today", "user_profile", "input"],
+        template=CONVERSATION_TEMPLATE + "\n\n" + zero_shot_template
+    )
+
+    return chat_db_prompt
